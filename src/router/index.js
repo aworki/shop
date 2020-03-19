ba12cf40736cca10bc1555dd6e93922fa4ff00b5
aworki <1224518406@qq.com>
2020-03-19 17:37:36 +0800

    login funchtion

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/login', component: Login },
+  { path: '/', redirect: '/login' },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // to表示将要访问的的路径
+  // from表示从哪哥路径来
+  // next表示放行（next（'/login'））
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
